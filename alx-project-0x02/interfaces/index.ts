0ca622a6a16eeefdd2f3500022bc8a427e6a5556
Joy <muirujoybackup@gmail.com>
2025-07-23 22:24:15 +0300

    added

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -71,3 +71,10 @@ export interface ButtonProps {
   children?: React.ReactNode;
   className?: string;
 }
+export interface CardProps {
+  title: string;
+  description?: string;
+  imageUrl?: string;
+  children?: React.ReactNode;
+  className?: string;
+}
