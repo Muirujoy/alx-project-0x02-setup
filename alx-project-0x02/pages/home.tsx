e6f42403353c1ccbb304090b939dc708a1d32ebd
Joy <muirujoybackup@gmail.com>
2025-07-23 18:00:39 +0300

    added

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,11 +1,26 @@
 // pages/home.tsx
 import React from "react";
+import Card from "@/components/common/Card";
 
 const HomePage = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
-      <p>This is the home page of our application.</p>
+    <div className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold mb-4">Welcome to the Home Page</h1>
+
+      <Card
+        title="Card One"
+        content="This is the content of the first card. It can be anything you'd like."
+      />
+
+      <Card
+        title="Card Two"
+        content="Here's some content for the second card. This shows reusability."
+      />
+
+      <Card
+        title="Card Three"
+        content="You can create as many cards as you want using the same component."
+      />
     </div>
   );
 };
