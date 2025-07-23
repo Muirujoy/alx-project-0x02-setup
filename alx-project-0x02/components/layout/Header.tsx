f7ffa7acfae24d8b6442a10f535565aa4fa18588
Joy <muirujoybackup@gmail.com>
2025-07-23 17:53:07 +0300

    added

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="bg-indigo-600 text-white text-center py-4 text-2xl font-bold">
+      Welcome to ALX Project 02
+    </header>
+  );
+};
+
+export default Header;
